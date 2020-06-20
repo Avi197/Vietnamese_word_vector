534b62ce39fec23619cb48fbbaa4d0f535afba26
avi197 <phamson11999977@gmail.com>
2020-06-20 13:26:48 +0700

    add word seg

---
+++ word_segmentation.py
@@ -0,0 +1,34 @@
+
+from vncorenlp import VnCoreNLP
+import file_path
+
+
+# vncorenlp -Xmx500m /home/pham/VnCoreNLP-master/VnCoreNLP-1.1.1.jar -p 9000 -a "wseg"
+
+
+def tokenized(infile, outfile):
+	count = 0
+	with VnCoreNLP(address='http://127.0.0.1', port=9000) as vncorenlp:
+		with open(infile, encoding='utf-8') as file:
+			with open(outfile, 'w', encoding='utf-8') as out:
+				for line in file:
+					if line:
+						try:
+							word_seg = vncorenlp.tokenize(line)
+							if not line:
+								print('fucked here')
+						except:
+							time.sleep(5)
+						for sent in word_seg:
+							seg = ' '.join(sent)
+							out.writelines(seg + '\n')
+						print('done line ' + str(count))
+					count += 1
+	print(f'done {infile}')
+
+# tokenized(file_path.Dantri_text, file_path.Dantri_tokenized)
+tokenized(file_path.Thanhnien_text, file_path.Thanhnien_tokenized)
+# tokenized(file_path.Tuoitre_text, file_path.Tuoitre_tokenized)
+# tokenized(file_path.Vnexpress_text, file_path.Vnexpress_tokenized)
+# tokenized(file_path.Vnn_text, file_path.Vnn_tokenized)
+# tokenized(file_path.Vtv_text, file_path.Vtv_tokenized)
